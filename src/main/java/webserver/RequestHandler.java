10d98a54f71c9d316ac9251eb3c1edb4b04fe203
tmdqja0705@naver.com <tmdqja0705@naver.com>
2023-05-15 10:03:31 +0900

    컨텐츠 타입, 리소스 경로 변수 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HeaderParser;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -22,26 +23,32 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-            String line = br.readLine();
-            String path = HeaderParser.getURL(line);
-            while (line != null && line.length() != 0) {
-                logger.debug(line);
-                line = br.readLine();
+            String firstHeaderLine = br.readLine();
+            String path = HeaderParser.getURL(firstHeaderLine);
+            String resourcePath = HeaderParser.getResourcePath(path);
+            String contentType = HeaderParser.getContentType(path);
+            logger.debug("Client Request = " + path);
+            while (!firstHeaderLine.equals("")) {
+                logger.debug(firstHeaderLine);
+                firstHeaderLine = br.readLine();
             }
-            logger.debug(path);
+            if(HeaderParser.checkRequestType(path)){
+                resourcePath = "/templates";
+                path = "/index.html";
+            }
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./src/main/resources/templates" + path).toPath());
-            response200Header(dos, body.length);
+            byte[] body = Files.readAllBytes(new File("./src/main/resources" +resourcePath + path).toPath());
+            response200Header(dos, body.length,contentType);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent,String contentType) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Type: "+contentType+"\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
